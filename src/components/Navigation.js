c851649abc7aae01bc930b666230937b26bbd4fb
MaryWenzel <wenzel.mary1993@gmail.com>
2021-04-13 00:07:55 -0500

    nav styling and burger menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,14 @@
 import React from "react";
 import logo from "../portfolio-name.png";
+// FONT AWESOME
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretSquareDown } from "@fortawesome/free-solid-svg-icons";
 
 const Navigation = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
       <a className="navbar-brand" href="#">
-        <img src={logo} alt="" />
+        <img className="logo" src={logo} alt="" />
       </a>
       <button
         className="navbar-toggler"
@@ -16,7 +19,7 @@ const Navigation = () => {
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span className="navbar-toggler-icon"></span>
+        <FontAwesomeIcon icon={faCaretSquareDown} style={{ color: "white" }} />
       </button>
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -28,7 +31,18 @@ const Navigation = () => {
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#">
-              Link
+              about me
+            </a>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              portfolio
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              contact
             </a>
           </li>
         </ul>
